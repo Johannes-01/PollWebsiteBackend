63668644ff7cb1cad8099ebf98621468c7837e74
MaxBaer <MaxBaer>
2023-06-16 13:27:03 +0200

    Changed LoginRequest

--- Webserver/Webserver/DTOs/LoginRequestDTO.cs
+++ Webserver/Webserver/DTOs/LoginRequestDTO.cs
@@ -3,14 +3,13 @@ using System.Security.Cryptography.X509Certificates;
 using Microsoft.EntityFrameworkCore;
 
 
+
 namespace Webserver.DTOs
 {
 	public class LoginRequestDto
 	{
 		public LoginRequestDto()
 		{
-			[Required]
-			public int LoginId { get; set; }
 
 			[Required]
 			public string Username { get; set; }
