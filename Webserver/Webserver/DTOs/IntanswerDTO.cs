baafb5b0a938b2d98aa3d68ff64eb1f33d63fb1f
MaxBaer <MaxBaer>
2023-06-16 13:24:41 +0200

    Added DTOs

---
+++ Webserver/Webserver/DTOs/IntanswerDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Webserver.Model;
+
+namespace Webserver.DTOs
+{
+
+	public class IntanswerDto
+	{
+		public IntanswerDto()
+		{
+			[Required]
+			public int IntanswerID {  get; set; }
+
+			public int Index { get; set; }
+
+			[Required]
+			public int Value { get; set; }
+		}
+	}
+}
---
+++ Webserver/Webserver/DTOs/LoginRequestDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Webserver.DTOs
+{
+	public class LoginRequestDto
+	{
+		public LoginRequestDto()
+		{
+			[Required]
+			public int LoginId { get; set; }
+
+			[Required]
+			public string Username { get; set; }
+
+			[Required]
+			public string Password { get; set; }
+		}
+	} 
+}
